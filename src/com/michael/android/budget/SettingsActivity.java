aeaf75ca9d32e55763b80142917b2403ece1579c
Andy Lew <ydnawel@gmail.com>
2012-06-04 13:01:43 -0700

    -added export results to email option in settings -sending the email does NOT work (need to figure out services)

--- src/com/michael/android/budget/SettingsActivity.java
+++ src/com/michael/android/budget/SettingsActivity.java
@@ -1,10 +1,17 @@
 package com.michael.android.budget;
 
+import java.net.URLEncoder;
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +22,8 @@ public class SettingsActivity extends Activity {
 		setContentView(R.layout.settings);
 		SharedPreferences settings = getSharedPreferences(DailyBudgetTrackerActivity.PREFS_NAME, 0);
 		updateBudgetText(settings.getInt(DailyBudgetTrackerActivity.BUDGET, 2000));
+		restorePreviousExports();
+		addExportListener();
 	}
 	
 	/**resets the whole app to out of box settings. Debug purposes*/
@@ -90,4 +99,100 @@ public class SettingsActivity extends Activity {
     	value = value + " " + Integer.toString(newMasBudget);
     	text.setText(value);
     }
+    
+    public void addExportListener() {
+    	final CheckBox check = (CheckBox) findViewById(R.id.export_check);
+    	check.setOnClickListener(new OnClickListener() {
+    		public void onClick(View v) {
+    			if( ((CheckBox) v).isChecked() ) {
+    				// send emails!
+    				String email = getEmail();
+    				if (email == null) {
+    					// Checkbox checked but no email address. Does not send emails.
+    					Toast no_email_msg = Toast.makeText(getApplicationContext(), "Please enter an email address.", Toast.LENGTH_SHORT);
+    					no_email_msg.show();
+    					
+    					check.setChecked(false);
+    					storeExportSettings(null, false);
+    				}
+    				else {
+    					// Checkbox checked and has an email address. Sends emails.
+    					Toast email_msg = Toast.makeText(getApplicationContext(), "Ok, I have an email address.", Toast.LENGTH_SHORT);
+    					email_msg.show();
+    					
+				        storeExportSettings(email, true);
+				        sendEmail();
+    				}
+    			}
+    			else {
+    				// stop sending emails!
+					Toast stop = Toast.makeText(getApplicationContext(), "Emails will not be sent.", Toast.LENGTH_SHORT);
+					stop.show();
+					
+	    			SharedPreferences settings = getSharedPreferences(DailyBudgetTrackerActivity.PREFS_NAME, 0);
+			        SharedPreferences.Editor editor = settings.edit();
+			        editor.putBoolean(DailyBudgetTrackerActivity.EXPORT, false);
+			        editor.commit();
+			        
+			        stopEmail();
+    			}
+    		}
+    	});
+    }
+    
+    public String getEmail() {
+    	EditText et = (EditText) findViewById(R.id.email);
+    	Editable email = et.getText();
+    	if (email.length() == 0) {
+    		return null;
+    	}
+    	return email.toString();
+    }
+    
+    public void storeExportSettings(String email, boolean sendEmails) {
+    	// Stores email and export preference settings.
+    	SharedPreferences settings = getSharedPreferences(DailyBudgetTrackerActivity.PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(DailyBudgetTrackerActivity.EMAIL, email);
+        editor.putBoolean(DailyBudgetTrackerActivity.EXPORT, sendEmails);
+        editor.commit();    }
+    
+    public void restorePreviousExports() {
+    	SharedPreferences settings = getSharedPreferences(DailyBudgetTrackerActivity.PREFS_NAME, 0);
+    	EditText email = (EditText) findViewById(R.id.email);
+    	email.setText(settings.getString(DailyBudgetTrackerActivity.EMAIL, null)); 
+    	
+    	CheckBox export = (CheckBox) findViewById(R.id.export_check);
+    	export.setChecked(settings.getBoolean(DailyBudgetTrackerActivity.EXPORT, false));
+    }
+
+	@Override
+	protected void onDestroy() {
+		// When the settings activity is destroyed, save whatever is in the email field.
+		super.onDestroy();
+		String email = getEmail();
+    	SharedPreferences settings = getSharedPreferences(DailyBudgetTrackerActivity.PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(DailyBudgetTrackerActivity.EMAIL, email);
+        editor.commit();
+	}
+	
+	public void sendEmail() {
+    	SharedPreferences settings = getSharedPreferences(DailyBudgetTrackerActivity.PREFS_NAME, 0);
+    	String email = settings.getString(DailyBudgetTrackerActivity.EMAIL, "");
+		String uriText =
+		    "mailto:" + email +
+		    "?subject=" + URLEncoder.encode("[Daily Budget Tracker] Yesterday's calorie history:") + 
+		    "&body=" + URLEncoder.encode("stuff");
+
+		Uri uri = Uri.parse(uriText);
+
+		Intent send_email = new Intent(Intent.ACTION_SENDTO);
+		send_email.setData(uri);
+		startActivity(send_email);
+	}
+	
+	public void stopEmail() {
+	}
+    
 }
