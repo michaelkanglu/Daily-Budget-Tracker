bca617eb9c684e88b443c52c0458d0d9b967eb51
Michael Lu <mklu2@illinois.edu>
2012-06-11 22:14:24 -0700

    Finished profile.xml skeleton

---
+++ src/com/michael/android/budget/Profile.java
@@ -0,0 +1,11 @@
+package com.michael.android.budget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Profile extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+}
